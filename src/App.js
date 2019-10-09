a892ad776aa4f2a8e351e02409150cddeaf2a69c
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-10-09 15:18:49 -0400

    finished checkpoint 11 example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import './App.css';
-import Tabs from './state/Tabs';
-
-const tabsProp = [
-  { name: 'First tab',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
-  { name: 'Second tab',
-    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
-  { name: 'Third tab',
-    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
-];
+import Accordion from './state/Tabs';
 
 class App extends React.Component {
   render() {
-    return <div>
-      <Tabs tabs={tabsProp} />
-    </div>
+    return (
+      <div>
+        <Accordion />
+      </div>
+    )
   }
 }
 
